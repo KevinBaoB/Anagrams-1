2356db0b249e0e1f399caaaaf2583253b05c9403
KevinBaoB <kevinbaobui@gmail.com>
2022-05-26 19:29:33 -0500

    Second

--- python/character_match.py
+++ python/character_match.py
@@ -1,5 +1,16 @@
 # Don't forget to run your tests!
 
 def is_character_match(string1, string2):
-	pass
 	# Your code here
+	res = True 
+	string1 = string1.lower()
+	string2 = string2.lower()
+	for i in string1:
+		if i in string2:
+			res = True
+		else: return False
+	return res
+			
+
+
+# print(is_character_match('charm', 'march'))

0f5f8efc361c3fedd89a017dc2e7a7b2fc80192a
KevinBaoB <kevinbaobui@gmail.com>
2022-05-26 19:31:44 -0500

    First

--- js/characterMatch.js
+++ js/characterMatch.js
@@ -1,3 +1,11 @@
-exports.isCharacterMatch = function(string1, string2) {
-
+exports.isCharacterMatch = function (string1, string2) {
+  string1 = string1.toLowerCase();
+  string2 = string2.toLowerCase();
+  for (let i = 0; i < string1.length; i++) {
+    if (string2.includes(string1[i])) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 };
